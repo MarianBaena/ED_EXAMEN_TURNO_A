03e61ecd2ba1f26fbee095e65793fee64e393ab6
Marian Baena <mae81b@gmail.com>
2021-01-11 17:29:17 +0100

    version final con javadoc

--- src/empleadoED_48819142d.java
+++ src/empleadoED_48819142d.java
@@ -1,17 +1,33 @@
 
 import java.util.*;
-
+/**
+ * Ejercicio práctico examen EVAL 1 ED
+ * 
+ * @author MARIAN BAENA DNI 48819142D
+ * @version 1.0
+ */
 public class EmpleadoED_48819142D {
 
     static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
-
+    /**
+     * Constructor con tres parámetros
+     * 
+     * @param nombreCompleto nombre completo en mayúsculas
+     * @param dni documento nacional de indentidad con letra y sin espacios 
+    */
     public EmpleadoED_48819142D(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
-
+    /**
+     * Calcula el salario bruto mensual en función del salrio base por hora y las horas extras
+     * El precio por hora d ela hora extra se detrmina con la constante FACTOR_HORA_EXTRA
+     * 
+     * @param horasExtra
+     * @return 
+     */
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
